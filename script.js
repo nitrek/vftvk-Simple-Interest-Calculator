79934644cf8dbfc235ee110d0ee3d557e98e6de7
nitrek <koolestnitesh@gmail.com>
2021-04-16 01:09:48 +0530

    fixes

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-function compute()
-{
-   var p = document.getElementById("principal").value;
-   if(p<=0)
-   {
-       alert("enter a positive number greater than zero");
-       document.getElementById("prinicipal").focus();
-       document.getElementById("prinicipal").click();
-       return null;
-   }
-   var r = document.getElementById("rate").value;
-   var y =document.getElementById("years").value;
-   console.log(y);
-   var interest = p*r*y/100;
-   console.log(p*r);
-   var ndate = new Date();
-   newdate = ndate.getFullYear();
-   newdate+= parseInt(y);
-   document.getElementById("result").innerHTML =  "<br>" + "<br>" + "If you deposit " + "<span class='highlight'>" + p + '</span>' +'<br>' + ' at an interest rate of ' + "<span class='highlight'>" + r + '</span>' +'%.' +'<br>' + 'You will recive an amount of ' + "<span class='highlight'>" +interest + '</span>' +'<br>' + 'In the year ' + "<span class='highlight'>" +newdate + '</span>';
+function compute() {
+    var p = document.getElementById("principal").value;
+    if (p <= 0) {
+        alert("enter a positive number greater than zero");
+        document.getElementById("principal").focus();
+        document.getElementById("principal").click();
+        return null;
+    }
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    console.log(y);
+    var interest = p * r * y / 100;
+    console.log(p * r);
+    var ndate = new Date();
+    newdate = ndate.getFullYear();
+    newdate += parseInt(y);
+    document.getElementById("result").innerHTML = "<br>" + "If you deposit " + "<span class='highlight'>" + p + '</span>' + '<br>' + ' at an interest rate of ' + "<span class='highlight'>" + r + '</span>' + '%.' + '<br>' + 'You will recive an amount of ' + "<span class='highlight'>" + interest + '</span>' + '<br>' + 'In the year ' + "<span class='highlight'>" + newdate + '</span>';
 }
-function ratechange()
-{
+
+function ratechange() {
     var rchng = document.getElementById("rate").value;
     document.getElementById("rate-displayer").innerHTML = rchng + '%';
 }
